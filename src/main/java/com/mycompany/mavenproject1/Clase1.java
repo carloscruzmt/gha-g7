361d3ee3d4f78734d08d78d1bc86783cc98d70d5
carloscruzmt <carlos.cruz.mt@correo.usa.edu.co>
2021-10-27 06:57:53 -0500

    primer commit del proyecto gha

---
+++ src/main/java/com/mycompany/mavenproject1/Clase1.java
@@ -0,0 +1,11 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author PosYo
+ */
+public class Clase1 {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/mycompany/mavenproject1/Clase1Test.java
@@ -0,0 +1,48 @@
+package com.mycompany.mavenproject1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author juvinao
+ */
+public class Clase1Test {
+    
+    public Clase1Test() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sumar method, of class Clase1.
+     */
+    @Test
+    public void testSumar() {
+        System.out.println("sumar");
+        Clase1 instance = new Clase1();
+        int result = instance.sumar(5, 4);
+        assertEquals(9, result);
+    }
+    
+}
+
+
